1a17b7a186dd4aac40027b1f75dcc16d47ef0b0f
SeaIceNX <SeaIce233@outlook.com>
2021-02-18 19:44:21 +0800

    Fix

---
+++ IronPythonRunner/Schedule.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+
+namespace IronPythonRunner
+{
+    public class Schedule
+    {
+        private Thread t { get; set; }
+        /// <summary>
+        /// 获得该scheduleid
+        /// </summary>
+        public int ScheduleID
+        {
+            get
+            {
+                return t.ManagedThreadId;
+            }
+        }
+        /// <summary>
+        /// 延时任务
+        /// </summary>
+        /// <param name="func">py函数</param>
+        /// <param name="delay">延时</param>
+        /// <param name="cycle">重复次数</param>
+        public Schedule(dynamic func, int delay, int cycle)
+        {
+            int tmp = 0;
+            t = new Thread(() =>
+            {
+                while (tmp != cycle)
+                {
+                    Thread.Sleep(delay);
+                    func();
+                    tmp++;
+                }
+            });
+            t.Start();
+        }
+        /// <summary>
+        /// 延时任务，重复次数无限
+        /// </summary>
+        /// <param name="func">函数</param>
+        /// <param name="delay">延时</param>
+        public Schedule(dynamic func, int delay)
+        {
+            t = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(delay);
+                    func();
+                }
+            });
+            t.Start();
+        }
+        public void Stop()
+        {
+            t.Abort();
+        }
+    }
+}
